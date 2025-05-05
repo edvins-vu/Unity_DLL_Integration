12a90b188d40def27fbc04eeb865577220f2b56e
Edvins Vecverdins-Udris <edvins.vecverdins@gmail.com>
2025-05-05 22:36:48 +0300

    Refactored APIResponseHandler
    From existing logic extracted seperate APIService and FactUIHandler, added package to use UniTask for Async calls

---
+++ Assets/Scripts/APIService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using API_Handler;
+using Cysharp.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public class APIService
+    {
+        private readonly APIManager _apiManager;
+
+        public APIService(APIConfig config)
+        {
+            HttpClient client = new HttpClient();
+            _apiManager = new APIManager(client, config);
+        }
+
+        public async UniTask<string> FetchFactsAsync(string endpointKey)
+        {
+            return await _apiManager.SendRequest(endpointKey, HttpMethod.Get);
+        }
+    }
+}
---
+++ Assets/Scripts/FactsUIHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+    public class FactUIHandler : MonoBehaviour
+    {
+        public TextMeshProUGUI responseText;
+
+        public void UpdateFacts(List<Fact> facts)
+        {
+            string formattedText = "Facts:\n";
+
+            foreach (Fact fact in facts)
+            {
+                formattedText += $"- {fact.attributes.body}\n";
+            }
+
+            responseText.text = formattedText;
+        }
+    }
+}
